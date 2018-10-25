aef0115c50c1f7b625826559b93d889ff8fd78a9
jason <jasonma886@gmail.com>
2018-10-25 14:31:01 +0800

    max product

---
+++ medium/maxProduct.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  let len = nums.length, first = nums[0]
+  let max = first, min = first
+  nums.forEach((item, idx) => {
+    if (idx === 0) {
+      return
+    }
+    if (item < 0) {
+      [max, min] = [min, max]
+    }
+    max = Math.max(item, item * max)
+    min = Math.min(item, item * min)
+    first = Math.max(first, max)
+  })
+  return first
+}
+
+console.log(maxProduct([3, 4, -1, 5, 67]));
